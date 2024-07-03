c27ab5dbb125b60390f12090dc43091e4dd354c1
Debashis Maharana <debashismaharana7854@gmail.com>
2024-07-03 19:34:09 +0530

    Get request happening

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.use(express.json());
+
+let courses=[
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'}
+];
+
+app.get('/courses', (req, res)=>{
+    res.json(courses);
+});
+
+app.post('/courses', (req, res)=>{
+    console.log(req.body);
+    const course = {
+       id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(courses);
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Server is Listening {http://localhost:${port}} `)
+});
+
+
